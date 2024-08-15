e674c911a3f6cec73d33210e80a5f9bf6759270d
Wan Guolin <wanguolin@gmail.com>
2024-08-15 13:15:14 +0000

    leaves seval syntax for import titles

--- fetch.py
+++ fetch.py
@@ -6,8 +6,10 @@ import requests
 import argparse
 import json
 import csv
+from datetime import datetime
 
 _lang = "en"
+_database_name = "english_listening_room"
 _output_folder = "downloads"
 _ted_talks_base_url = "https://www.ted.com/talks/"
 _quick_list = "https://www.ted.com/talks/quick-list"
@@ -241,7 +243,10 @@ def download_stats():
     )
 
 
-def download(quality):
+def download(quality: str):
+    if quality not in ["low", "medium", "1080p"]:
+        print("Invalid quality specified. Choose from 'low', 'medium', '1080p'.")
+        exit(1)
     quality_column_map = {
         "low": "download_low",
         "medium": "download_medium",
@@ -270,13 +275,138 @@ def download(quality):
                 download_links.append(value)
 
 
+def export_sql(
+    database_name: str = _database_name, meta_csv_path: str = "meta.csv"
+) -> str:
+    def scan_meta_max_field_length(csv_path: str) -> tuple:
+        max_varchar_length = {
+            "Published": 0,
+            "Title": 0,
+            "Event": 0,
+            "Duration": 0,
+            "Details": 0,
+        }
+
+        df = pd.read_csv(csv_path)
+        df = df.fillna("")
+
+        sql_insert_statements = []
+        for _, row in df.iterrows():
+            for field in max_varchar_length:
+                field_value = str(row[field])
+                max_varchar_length[field] = max(
+                    max_varchar_length[field], len(field_value)
+                )
+            published_date = (
+                lambda d: (
+                    datetime.strptime(d, "%b %Y").strftime("%Y-%m-%d")
+                    if isinstance(d, str) and d != ""
+                    else "NULL"
+                )
+            )(row["Published"])
+            title = row["Title"].replace("'", "''")
+            event = row["Event"]
+            duration = row["Duration"].replace("'", "''")
+            download_links = {
+                "low": row["download_low"],
+                "medium": row["download_medium"],
+                "1080p": row["download_1080p"],
+            }
+            details_link = row["Details"].replace("'", "''")
+            download_links_json = json.dumps(download_links).replace("'", "''")
+            insert_sql = (
+                f"INSERT INTO ted_talks_meta (published, title, event, duration, download_links, details_link) "
+                f"VALUES ("
+                f"'{published_date}', "
+                f"'{title}', "
+                f"'{event}', "
+                f"'{duration}', "
+                f"'{download_links_json}', "
+                f"'{details_link}');"
+            )
+            sql_insert_statements.append(insert_sql)
+
+        return max_varchar_length, "\n".join(sql_insert_statements)
+
+    max_varchar_length, insert_ted_talks_meta_sql = scan_meta_max_field_length(
+        meta_csv_path
+    )
+
+    create_db_sql = f"""
+    DO $$
+    BEGIN
+        IF EXISTS (SELECT 1 FROM pg_database WHERE datname = '{database_name}') THEN
+            EXECUTE 'DROP DATABASE {database_name}';
+        END IF;
+        EXECUTE 'CREATE DATABASE {database_name}';
+    END
+    $$;
+    """
+
+    create_table_sql = f"""
+    CREATE TABLE ted_talks_meta (
+        id SERIAL PRIMARY KEY,
+        published DATE,
+        title VARCHAR({max_varchar_length['Title']}) UNIQUE NOT NULL,
+        event VARCHAR({max_varchar_length['Event']}),
+        duration VARCHAR({max_varchar_length['Duration']}),
+        download_links JSONB,
+        details_link VARCHAR({max_varchar_length['Details']}),
+        details_content JSONB
+    );
+    """
+
+    optimize_table_sql = """
+    CREATE INDEX idx_details_content_keywords ON english_listening_classroom USING GIN ((details_content->'keywords') jsonb_path_ops);
+    """
+
+    sql_file_path = "import_ted_talks.sql"
+
+    with open(sql_file_path, "w") as sql_file:
+        sql_file.write(create_db_sql)
+        sql_file.write(create_table_sql)
+        sql_file.write(optimize_table_sql)
+        sql_file.write(insert_ted_talks_meta_sql)
+
+        for json_file in [
+            f
+            for f in os.listdir(".")
+            if f.endswith(".json") and not f.endswith("_sub_en.json")
+        ]:
+            with open(json_file, "r") as file:
+                data = json.load(file)
+                title = os.path.splitext(os.path.basename(json_file))[0]
+
+                keywords = data.get("keywords", "")
+                keywords_array = [keyword.strip() for keyword in keywords.split(",")]
+
+                if "details_content" not in data:
+                    data["details_content"] = {}
+                data["details_content"]["keywords"] = keywords_array
+
+                details_content_json = json.dumps(data["details_content"]).replace(
+                    "'", "''"
+                )
+                update_sql = f"""
+                UPDATE english_listening_classroom 
+                SET details_content = '{details_content_json}' 
+                WHERE title = '{title}';
+                """
+                sql_file.write(update_sql)
+
+    print(f"The sql file is saved to {sql_file_path}")
+
+
 def main():
     parser = argparse.ArgumentParser(
         description="A script to fetch TED Talks meta data and details."
     )
     parser.add_help = True
     parser.add_argument(
-        "--download-meta", action="store_true", help="Fetch meta data from TED Talks"
+        "--download-meta",
+        action="store_true",
+        default=False,
+        help="Fetch meta data from TED Talks",
     )
     parser.add_argument(
         "--download-details",
@@ -297,13 +427,20 @@ def main():
     parser.add_argument(
         "--download-stats",
         action="store_true",
+        default=False,
         help="Calculate download success statistics",
     )
     parser.add_argument(
         "--download-audio",
         type=str,
-        default="low",
+        default=None,
         help="Convert meta.csv into download_<quality>.lst. Default: low quality. Usage: --output-download-list low, medium, high",
+    )
+    parser.add_argument(
+        "--export-sql",
+        action="store_true",
+        default=False,
+        help="Export meta.csv and json files to SQL database",
     )
 
     args = parser.parse_args()
@@ -322,8 +459,10 @@ def main():
         fetch_meta()
         fetch_ted_details_from_meta()
         download_stats()
-    elif args.download_audio:
+    elif args.download_audio is not None:
         download(args.download_audio)
+    elif args.export_sql:
+        export_sql()
 
 
 if __name__ == "__main__":
